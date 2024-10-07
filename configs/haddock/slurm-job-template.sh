9b983e6e25aed0d701ae6d4d2a387bc07e3a21cd
Wouter van den Broeke <w.vanden.broeke@student.vu.nl>
2024-10-07 22:22:17 +0200

    Explanation

--- configs/haddock/slurm-job-template.sh
+++ configs/haddock/slurm-job-template.sh
@@ -22,7 +22,9 @@ for ((i = 0 ; i < $ntasks ; i++)); do
     RUN_DIR=run$i NCORES=${cpus_per_task} envsubst < ${cfg_file} > ${job_name}-$i.cfg
 done
 
-# Run the haddock3 workflow
+# Run the haddock3 workflow, %t is the index of the task (0 to N) being executed.
+# $SLURM_PROCID is this same index, but of the actual worker job, after it has started,
+# which is what allows us to get it inside the bash command.
 srun --output=${shared_dir}/out/${job_name}-%t.out  bash -c 'haddock3 ${job_name}-$SLURM_PROCID.cfg'
 
 # Cleanup
