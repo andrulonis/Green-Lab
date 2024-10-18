d80e4ac45365139841f8f418c1727059d1357f8f
Wouter van den Broeke <vandenbroekew@gmail.com>
2024-10-18 15:54:46 +0200

    Updated QQ plots

--- r-scripts/test-data.R
+++ r-scripts/test-data.R
@@ -13,34 +13,55 @@ job_types <- c("docking-protein-DNA",
                "cyclise-peptide")
 modes <- c("sequential", "parallel")
 metrics <- c("AvgCPU", "AvgMem", "ExecTime", "TotalEnergy")
+metrics_labels <- c("CPU utilisation (%)", "Memory usage (GiB)", "Execution time (s)", "Energy usage (kJ)")
+df_total$AvgMem = df_total$AvgMem / 2^30
+df_total$ExecTime = df_total$ExecTime / 10^3
+df_total$TotalEnergy = df_total$TotalEnergy / 10^3
 
 # QQ-plot
 
-png(
-    paste(
-      dirname(rstudioapi::getSourceEditorContext()$path),
-      "/out/plots/qqplots.png",
-      sep = ""
-    ),
-    width=1600,
-    height=1200
-)
-par(mfrow=c(4,6))
+# png(
+#     paste(
+#       dirname(rstudioapi::getSourceEditorContext()$path),
+#       "/out/plots/qqplots.png",
+#       sep = ""
+#     ),
+#     width=1600,
+#     height=1200
+# )
+par(mfrow=c(4,6), oma=c(1, 3, 5, 0), mar=c(1.75, 1.75, 1.75, 1.75))
+
 
 for (metric in metrics) {
   for (job in job_types) {
     for (mode in modes) {
       data <- df_total[[metric]][df_total$JobType == job & df_total$Mode == mode]
+      
       qqPlot(
         data,
-        main = paste("QQ-Plot of", metric, "\nfor", job, "\n", mode),
-        ylab = colnames(df_total)[result]
+        ylab = "",
+        main = NULL
       )
     }
   }
 }
 
-dev.off()
+title("QQ-plots of ", outer = TRUE, line = 3, cex.main = 2)
+
+for (row in seq_along(metrics_labels)) {
+  mtext(metrics_labels[length(metrics_labels) + 1 - row], side = 2, line = 1, outer = TRUE, at = 0.130  + 0.25 * (row - 1), font = 2, cex = 1)
+}
+for (row in seq_along(job_types)) {
+  mtext(job_types[row], side = 3, line = 1, outer = TRUE, at = 0.175 + (row - 1) * 0.33, font = 2, cex = 1)
+}
+
+for (mode in seq(1, 6, 2)) {
+  mtext(modes[1], side = 3, line = -1, outer = TRUE, at = 0.08 + 0.1675 * (mode - 1), font = 2, cex = 1)
+  mtext(modes[2], side = 3, line = -1, outer = TRUE, at = 0.25 + 0.1675 * (mode - 1), font = 2, cex = 1)
+}
+
+
+# dev.off()
 
 # Shapiro-Wilk
 filePath = paste(
